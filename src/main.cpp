4a2ffac7613067d6940085db3ed9dba65dd7e370
MarkGeorge2004 <markgeorge626@gmail.com>
2025-12-05 19:47:21 +0200

    2

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,91 @@
 #include <Arduino.h>
-int x;
-int y;
-int z;
-# define relay_pin 23
-# define iR1_pin 2
-# define iR2_pin 3
-# define Msensor_pin 5  // NPN mode 
-int count=0;
-void on_relay(){digitalWrite(relay_pin,HIGH);}
-void off_relay(){digitalWrite(relay_pin,LOW);}
+#define relay_pin 23
+#define iR1_pin 2
+#define iR2_pin 3
+#define Msensor_pin 5 // NPN mode
+#define Switch_pin 6
 
-bool sort_1(){
-  int sort= digitalRead(iR2_pin);
-  return (sort== HIGH);
-}
-bool sort_2(){
-  int sort= digitalRead(Msensor_pin);
-  return (sort== HIGH);
-}
+#define forwardTime 1000
+#define BackwardTime 1000
 
-void feeder(){
-  int part_Detected = digitalRead(iR1_pin);
-  if(part_Detected==LOW){       //  parts infront of the sensor 
-      on_relay();                           // on relay     
-      Serial.println("relay is on");         
-      count++;                               // count 1
+class part
+{
+public:
+  bool isWhite;
+  bool isBase;
+};
+
+part currentPart;
+
+void sort(void *parameter)
+{
+  if (digitalRead(Switch_pin) == LOW)
+  {
+    currentPart.isBase = (digitalRead(Msensor_pin) == HIGH);
+    currentPart.isWhite = (digitalRead(iR2_pin) == HIGH);
   }
-  else if(part_Detected==HIGH){              // off relay  
-      off_relay();
-      count++;
-  }
-
-}
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(relay_pin,OUTPUT);
-  pinMode(iR1_pin,INPUT);
-  pinMode(iR2_pin,INPUT);
-  pinMode(Msensor_pin,INPUT);
-}
-
-void loop() {
-  on_relay();    // as defult on 
-  feeder();  
-  if(count==4){
-    off_relay();  // relay back to his own place
-  }                      
-
-  Serial.println("relay is off");
   // sorting
-  if (sort_1()){Serial.println("White");}   
-  else {Serial.println("Black");}
-  if (sort_2()){Serial.println("Metal");}
-  else{Serial.println("not metal");}
+  if (currentPart.isWhite)
+  {
+    Serial.println("White");
+  }
+  else
+  {
+    Serial.println("Black");
+  }
+  if (currentPart.isBase)
+  {
+    Serial.println("Base");
+  }
+  else
+  {
+    Serial.println("Lid");
+  }
 }
 
+void feed(void *parameter)
+{
+  bool part_Detected = (digitalRead(iR1_pin) == LOW);
+  bool no_part_Sorting = (digitalRead(Switch_pin) == HIGH);
+  if (no_part_Sorting && part_Detected)
+  {
+    digitalWrite(relay_pin, LOW);
+    delay(BackwardTime);
+    digitalWrite(relay_pin, HIGH);
+    delay(forwardTime);
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(relay_pin, OUTPUT);
+  digitalWrite(relay_pin, HIGH);
+
+  pinMode(iR1_pin, INPUT);
+  pinMode(iR2_pin, INPUT);
+  pinMode(Msensor_pin, INPUT);
+  delay(forwardTime);
+  Serial.println("ready");
+
+  xTaskCreate(
+      feed,   // Function name of the task
+      "feed", // Name of the task (e.g. for debugging)
+      2048,   // Stack size (bytes)
+      NULL,   // Parameter to pass
+      1,      // Task priority
+      NULL    // Task handle
+  );
+  xTaskCreate(
+      sort,   // Function name of the task
+      "sort", // Name of the task (e.g. for debugging)
+      2048,   // Stack size (bytes)
+      NULL,   // Parameter to pass
+      1,      // Task priority
+      NULL    // Task handle
+  );
+}
+
+void loop()
+{
+}
